a2fe0c9906f215acc6465edd9c778d92086ad2e1
Chen Win Sheng <alex.chen@webcomm.com.tw>
2020-03-26 13:52:48 +0800

    automate the generation of azure environment

--- infra/generate-azure-environment.ps1
+++ infra/generate-azure-environment.ps1
@@ -1,28 +1,28 @@
-$kubernetesResourceGroup="wisco-ipsum" # needs to be unique to your subscription
-$acrName='wiscoipsumacr' #must conform to the following pattern: '^[a-zA-Z0-9]*$
-$aksClusterName='wisco-ipsum-cluster'
+# Declare variables
+# needs to be unique to your subscription
+$kubernetesResourceGroup='DockerAzureTest' 
+#must conform to the following pattern: '^[a-zA-Z0-9]*$
+$acrName='DockerAzureTestRegistry007'
+$aksClusterName='myAKSCluster'
 $location = 'eastus'
-$numberOfNodes = 1 # In production, you're going to want to use at least three nodes.
+$vnetName = 'DockerAzureTest-vnet'
+# In production, you're going to want to use at least three nodes.
+$numberOfNodes = 1
 
+#create a resource group
 az group create -l $location -n $kubernetesResourceGroup
 
-az acr create --resource-group $kubernetesResourceGroup --name $acrName --sku Standard --location $location
+#create a container registry
+az acr create --resource-group $kubernetesResourceGroup --name $acrName --sku Basic --location $location
 
-$sp= az ad sp create-for-rbac --skip-assignment | ConvertFrom-Json
-$appId = $sp.appId
-$appPassword = $sp.password
-
-Start-Sleep -Seconds 120
-
+#get ACR ID
 $acrID=az acr show --resource-group $kubernetesResourceGroup --name $acrName --query "id" --output tsv
 
-az role assignment create --assignee $appId --scope $acrID --role acrpull
+#create AKS cluster
+az aks create --resource-group $kubernetesResourceGroup --name $aksClusterName --node-count $numberOfNodes --enable-addons monitoring --generate-ssh-keys --location $location
 
-az aks create `
-   --resource-group $kubernetesResourceGroup `
-   --name $aksClusterName `
-   --node-count $numberOfNodes `
-   --service-principal $appId `
-   --client-secret $appPassword `
-   --generate-ssh-keys `
-   --location $location
+#AKS cluster configured to authenticate with the ACR registry
+az aks update -n $aksClusterName -g $kubernetesResourceGroup --attach-acr $acrID
+
+#creates a default virtual network named DockerAzureTest-vnet with one subnet named default
+az network vnet create --name $vnetName --resource-group $kubernetesResourceGroup --subnet-name default
