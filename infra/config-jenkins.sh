de0fd4057103f575a12dc33008b8efd57a363557
Chen Win Sheng <alex.chen@webcomm.com.tw>
2020-03-26 14:20:38 +0800

    update config-jenkins.sh

--- infra/config-jenkins.sh
+++ infra/config-jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Install docker ce
+#Install docker ce
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -8,19 +8,19 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install docker-ce -y
 
-Install Azure CLI
+#Install Azure CLI
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
 sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
 
-Install Kubectl
+#Install Kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl	
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version –client
 
-Install .Net Core
+#Install .Net Core
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
