3cc65d4b2318c81377bebb5dee46f4e5f491cf24
Chen Win Sheng <alex.chen@webcomm.com.tw>
2020-03-26 16:00:38 +0800

    change config-jenkins.sh

--- infra/config-jenkins.sh
+++ infra/config-jenkins.sh
@@ -9,10 +9,10 @@ sudo apt-get update
 sudo apt-get install docker-ce -y
 
 #Install Azure CLI
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+#echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+#sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+#sudo apt-get install apt-transport-https
+#sudo apt-get update && sudo apt-get install azure-cli
 
 #Install Kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
